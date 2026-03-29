1f8e82bd5422d1f067956df8a5510fbcc24f7ace
Renmaps <renmaps@gmail.com>
2026-03-29 17:41:06 -0600

    Fix import path for ensureDatabaseReady

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { sql } from "bun";
-import { ensureDatabaseReady } from "./src/db/init";
+import { ensureDatabaseReady } from "./db/init";
 
 console.log(`API escuchando`);
 await ensureDatabaseReady();
