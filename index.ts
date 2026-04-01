51622e2d70f9cf12321ef43561947d8a7599f22d
Renmaps <renmaps@gmail.com>
2026-03-31 19:28:57 -0600

    Update index.ts

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ const server = Bun.serve({
       return new Response("OK->");
     }
     const headers = {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "http://localhost:5173",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     };
